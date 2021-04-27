a588b864873ab03d2cca51c69fd25eeb7f29e5d0
chris099 <kangjoon@bu.edu>
2021-04-27 17:55:01 -0400

    update for repeatability

--- 01C_COVdownload.R
+++ 01C_COVdownload.R
@@ -1,12 +1,11 @@
----
-title: "01C_COVdownload"
-author: "Nia Bartolucci; Cameron Reimer; Kangjoon Cho; Zhenpeng Zuo"
-date: "4/12/2021"
-output: html_document
----
 
+#title: "01C_COVdownload"
+#author: "Nia Bartolucci; Cameron Reimer; Kangjoon Cho; Zhenpeng Zuo"
+#date: "4/12/2021"
+#output: html_document
 
-```{r}
+  
+
 # install neonUtilities install
 #install.packages("neonUtilities") 
 #install.packages('BiocManager')
@@ -18,14 +17,9 @@ ym <- format(Sys.Date(),"%Y-%m")
 
 # load neonUtilities
 source("00C_Library+Directory_Setting.R")
-```
-
-
-
-```{r}
 
 Tmp <- loadByProduct(dpID="DP1.00002.001", site=c("BART","KONZ","OSBS","SRER"),
-              startdate="2020-01", enddate=ym, check.size=F)
+                     startdate="2020-01", enddate=ym, check.size=F)
 temp_data = Tmp[["SAAT_30min"]]
 
 newFilename <- sprintf("%s.Rdata","Air_Temperature")
@@ -33,11 +27,9 @@ newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 save(temp_data, file = newFilename)
 
 rm(Tmp,temp_data)
-```
 
-```{r}
 precip <- loadByProduct(dpID="DP1.00006.001", site=c("BART","KONZ","OSBS","SRER"),
-              startdate="2020-01", enddate=ym, check.size=F)
+                        startdate="2020-01", enddate=ym, check.size=F)
 precip_data = precip[["PRIPRE_30min"]]
 rm(precip)
 
@@ -45,20 +37,16 @@ newFilename <- sprintf("%s.Rdata","Precipitation")
 newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 save(precip_data, file = newFilename)
 rm(precip, precip_data)
-```
 
-```{r}
 swlw <- loadByProduct(dpID="DP1.00023.001", site=c("BART","KONZ","OSBS","SRER"),
-              startdate="2020-01", enddate=ym, check.size=F)
+                      startdate="2020-01", enddate=ym, check.size=F)
 swlw_data = swlw[["SLRNR_30min"]]
 
 newFilename <- sprintf("%s.Rdata","Radiance")
 newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 save(swlw_data, file = newFilename)
 rm(swlw,swlw_data)
-```
 
-```{r}
 #wind <- loadByProduct(dpID="DP1.00001.001", site=c("BART","KONZ","OSBS","SRER"),
 #              startdate="2020-01", enddate=ym, check.size=F)
 #
@@ -66,22 +54,18 @@ rm(swlw,swlw_data)
 #newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 #save(wind, file = newFilename)
 #rm(wind)
-```
 
-```{r}
 #hum <- loadByProduct(dpID="DP1.00098.001", site=c("BART","KONZ","OSBS","SRER"),
 #              startdate="2020-01", enddate=ym, check.size=F)
 #newFilename <- sprintf("%s.Rdata","Humidity")
 #newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 #save(hum, file = newFilename)
 #rm(hum)
-```
 
-```{r}
 #baro <- loadByProduct(dpID="DP1.00004.001", site=c("BART","KONZ","OSBS","SRER"),
 #              startdate="2020-01", enddate=ym, check.size=F)
 #newFilename <- sprintf("%s.Rdata","Pressure")
 #newFilename <- paste(dataPath, newFilename, sep="", collapse = NULL)
 #save(baro, file = newFilename)
 #rm(baro)
-```
+
